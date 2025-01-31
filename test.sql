7b78987b127d8d32ea4d4ffbdf6e5d61f8ee293d
Mark Wilcock <mark.wilcock@zomalex.co.uk>
2025-01-31 08:55:51 +0000

    more mssql format tests

--- test.sql
+++ test.sql
@@ -9,3 +9,13 @@ WHERE ps.AdmittedDate >= '2024-02-27';
 select top 10
     *
 from PricePaidSW12
+
+-- Select rows from a Table or View 'patientstay' in schema 'dbo'
+SELECT *
+FROM dbo.PatientStay ps
+WHERE ps.Hospital = 'PRUH'
+
+SELECT
+    *
+FROM
+    PatientStay ps
