94037e93b446cdbd3851040c2e6a45ae22711557
Mark Wilcock <mark.wilcock@zomalex.co.uk>
2025-02-11 14:40:38 +0000

    updated and re-ordered

--- Chinook Exercise.sql
+++ Chinook Exercise.sql
@@ -17,10 +17,6 @@ select * from Customer c
 
 -- Which customers have the initials LK?
 
-/*
-Which employees were born in the 1960s?  Show only the EmployeeId, FirstName, LastName and BirthDate columns
-Note: Define a date value as with the region independent format 'yyyy-mm-dd' e.g. '1969-12-31'
-*/
 
 -- Which are the corporate customers i.e. those with a value, not NULL, in the Company column?
 
@@ -28,22 +24,29 @@ Note: Define a date value as with the region independent format 'yyyy-mm-dd' e.g
 
 -- When was the oldest employee born?  Who is that?
 
--- List the albums in alphabetical order of Title
-
--- List 10 albums and their artist.  Order by album title.
-
 -- List the 10 latest invoices. Include the InvoiceId, InvoiceDate and Total
 -- Then  also include the customer full name (first and last name together)
 
+-- List the customers who have spent more than £45
 
 -- List the City, CustomerId and LastName of all customers in Paris and London, 
 -- and the Total of their invoices
 	
 -- Show all details about customer Michelle Brooks.  List salient details of her invoices.
 
+/*
+Which employees were born in the 1960s?  Show only the EmployeeId, FirstName, LastName and BirthDate columns
+Note: Define a date value as with the region independent format 'yyyy-mm-dd' e.g. '1969-12-31'
+*/
+
+
 -- List countries, and the number of customers and the total invoiced amount
 -- Order  high to low in terms of the number of customers
 
 -- What are the top 10 most popular artists in terms of number of tracks bought by customers?
+
+-- List the albums in alphabetical order of Title
+
+-- List 10 albums and their artist.  Order by album title.
 
 
